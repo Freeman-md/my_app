c2b3155f7d8e2736ff47b4bd5609a4d4a3cbbef7
Freeman-md <freemanmadudili@gmail.com>
2021-08-28 14:40:02 +0100

    Images & Assets

--- lib/main.dart
+++ lib/main.dart
@@ -18,16 +18,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'Hello Ninjas!',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-            ),
-        ),
+        child: Image.network('https://images.unsplash.com/photo-1534353436294-0dbd4bdac845?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
